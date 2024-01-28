2996c8f3ae16a8844af21c4571f80e725c02990f
AARADHYAPATEL <157728564+AARADHYAPATEL@users.noreply.github.com>
2024-01-28 13:48:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,35 @@
 from flask import Flask, render_template, request
 from googletrans import Translator
+from binary_converter import binary_addition
+from binary_converter import binary_subtraction
+from binary_converter import binary_multiplication
+from binary_converter import binary_division
 
 application = Flask(__name__, template_folder='templates')
+
+
+@application.route('/binary-conversion', method=['POST'])
+def Binary_operation():
+    if request.method == 'POST':
+        binary_input1 = request.form.get('binary_input1')
+        binary_input2 = request.form.get('binary_input2')
+        op = request.form.get('op')
+
+        if op == 'addition':
+            res = binary_addition(binary_input1, binary_input2)
+        elif op == 'subtraction':
+            res = binary_subtraction(binary_input1, binary_input2)
+        elif op == 'Multiplication':
+            res = binary_multiplication(binary_input1, binary_input2)
+        elif op == 'Division':
+            Quo, rem = binary_division(binary_input1, binary_input2)
+            res = f"Quotient: {Quo}, Remainder: {rem}"
+        else:
+            res = "Invalid Operation"
+
+        return render_template('binary_conversion.html', res=res)
+
+    return render_template('binary_conversion.html')
 
 
 @application.route('/')
@@ -18,6 +46,7 @@ def translate():
     translation_res = translator.translate(text_to_translate, dest=target_language)
 
     return render_template('index.html', translation_res=translation_res.text)
+
 
 
 if __name__ == '__main__':
